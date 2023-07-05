e6b63cc92544e09e6e229a7edf28688a978c020c
Maria <mariale171001@gmail.com>
2023-07-05 19:26:53 +0200

    update: add public routes to authMiddleware config

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware();
+export default authMiddleware({
+  publicRoutes: ["/api/:path*"]
+});
 
 // https://clerk.com/docs/nextjs/get-started-with-nextjs
 
