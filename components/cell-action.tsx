0302d084d5868d3df1d21f5d7ee4a72d1ae100ed
Maria <mariale171001@gmail.com>
2023-07-06 16:13:09 +0200

    create: reusable cell-action.tsx

---
+++ components/cell-action.tsx
@@ -0,0 +1,90 @@
+import axios from "axios";
+import { useState } from "react";
+import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react";
+import { toast } from "react-hot-toast";
+import { useParams, useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import { 
+  DropdownMenu, 
+  DropdownMenuContent, 
+  DropdownMenuItem, 
+  DropdownMenuLabel, 
+  DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
+import { AlertModal } from "@/components/modals/alert-modal";
+
+interface Entity {
+    id: string;
+  }
+
+interface CellActionProps<T> {
+  data: T;
+  entity: string;
+  entities: string;
+}
+
+export const CellAction = <T extends Entity>({ 
+    data, 
+    entity, 
+    entities,
+}: CellActionProps<T>) => {
+  const router = useRouter();
+  const params = useParams();
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const onConfirm = async () => {
+    try {
+      setLoading(true);
+      await axios.delete(`/api/${params.storeId}/${entities}/${data.id}`);
+      toast.success(`${entity} deleted.`);
+      router.refresh();
+    } catch (error) {
+      toast.error(`Make sure you removed all products related to ${entity} first.`);
+    } finally {
+      setOpen(false);
+      setLoading(false);
+    }
+  };
+
+  const onCopy = (id: string) => {
+    navigator.clipboard.writeText(id);
+    toast.success(`${entity} ID copied to clipboard.`);
+  };
+
+  return (
+    <>
+      <AlertModal 
+        isOpen={open} 
+        onClose={() => setOpen(false)}
+        onConfirm={onConfirm}
+        loading={loading}
+      />
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="h-8 w-8 p-0">
+            <span className="sr-only">Open menu</span>
+            <MoreHorizontal className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+                onClick={() => onCopy(data.id)}
+            >
+            <Copy className="mr-2 h-4 w-4" /> Copy Id
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            onClick={() => router.push(`/${params.storeId}/${entities}/${data.id}`)}
+          >
+            <Edit className="mr-2 h-4 w-4" /> Update
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setOpen(true)}>
+            <Trash className="mr-2 h-4 w-4" /> Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+}
